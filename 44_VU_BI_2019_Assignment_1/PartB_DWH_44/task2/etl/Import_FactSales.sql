6c0e11e9516256bc148de0f822e4af13050fe480
Jakob Pinterits <jakob.pinterits@gmail.com>
2019-11-30 14:13:21 +0100

    Temporary tables are now deleted if they exist

--- 44_VU_BI_2019_Assignment_1/PartB_DWH_44/task2/etl/Import_FactSales.sql
+++ 44_VU_BI_2019_Assignment_1/PartB_DWH_44/task2/etl/Import_FactSales.sql
@@ -1,4 +1,5 @@
-CREATE TEMPORARY TABLE tmp AS 
+DROP TABLE IF EXISTS tmp;
+CREATE TEMPORARY TABLE tmp AS
 SELECT h.SalesOrderNumber,
 CONCAT("SOL", h.SalesOrderID, "-", d.SalesOrderDetailID) AS SalesOrderLineNumber,
 h.CustomerID,
@@ -7,7 +8,7 @@ h.ShipToAddressID,
 h.BillToAddressID,
 sm.Name as ShipmentMethod,
 d.UnitPrice,
-case 
+case
 	WHEN d.OrderQty >= 10 AND pc.ParentCategoryID = 3 THEN 10 * d.UnitPrice * d.OrderQty
 	WHEN d.OrderQty >= 5 AND pc.ParentCategoryID = 3 THEN 5 * d.UnitPrice * d.OrderQty
 	WHEN d.OrderQty >= 10 AND pc.ParentCategoryID = 4 THEN 11 * d.UnitPrice * d.OrderQty
@@ -17,7 +18,7 @@ END AS Discount,
 d.OrderQty as OrderQuantity,
 # OrderLineTotal is generated on insert
 # OrderLineProfit is missing
-case 
+case
 	WHEN a.CountryRegion = "United States" THEN 8
 	WHEN a.CountryRegion = "Canada" THEN 13
 	WHEN a.CountryRegion = "United Kingdom" THEN 17.5
@@ -41,7 +42,7 @@ h.ShipDate > h.DueDate as IsLateShipment,
 p.StandardCost
 FROM BI_OLTP_44.TB_SalesOrderHeader h
 LEFT JOIN BI_OLTP_44.TB_SalesOrderDetail d
-ON h.SalesOrderID = d.SalesOrderID 
+ON h.SalesOrderID = d.SalesOrderID
 LEFT JOIN BI_OLTP_44.TB_ShipMethod sm
 ON h.ShipMethodID = sm.ShipMethodID
 LEFT JOIN BI_OLTP_44.TB_Product p
@@ -53,13 +54,13 @@ ON a.AddressID = h.ShipToAddressID
 LEFT JOIN BI_OLAP_44.DM_Product prod
 ON d.ProductID = prod.ProductID;
 
-ALTER TABLE tmp ADD COLUMN OrderLineTotal DECIMAL GENERATED ALWAYS 
+ALTER TABLE tmp ADD COLUMN OrderLineTotal DECIMAL GENERATED ALWAYS
 AS (`OrderQuantity` * `UnitPrice` - `Discount`) STORED;
 
-ALTER TABLE tmp ADD COLUMN TaxAmount DECIMAL GENERATED ALWAYS 
+ALTER TABLE tmp ADD COLUMN TaxAmount DECIMAL GENERATED ALWAYS
 AS (`Tax` * `OrderLineTotal`) STORED;
 
-ALTER TABLE tmp ADD COLUMN OrderLineProfit DECIMAL GENERATED ALWAYS 
+ALTER TABLE tmp ADD COLUMN OrderLineProfit DECIMAL GENERATED ALWAYS
 AS (`OrderLineTotal` - `OrderQuantity` * `StandardCost`) STORED;
 
 INSERT INTO BI_OLAP_44.DM_FactSales
