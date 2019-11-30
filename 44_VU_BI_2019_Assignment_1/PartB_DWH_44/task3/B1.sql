42e949ced796cccca66768e6163e0077f241ecaf
Jakob Pinterits <jakob.pinterits@gmail.com>
2019-11-30 15:34:38 +0100

    implemented query B1

--- 44_VU_BI_2019_Assignment_1/PartB_DWH_44/task3/B1.sql
+++ 44_VU_BI_2019_Assignment_1/PartB_DWH_44/task3/B1.sql
@@ -1,22 +1,19 @@
-SELECT ProductTopCategory, COUNT() as ProductRank, ProductName
-FROM DM_FactSales fs
-LEFT JOIN DM_Product p
-ON fs.ProductID = p.ProductID;
-
-
-
-WHERE
+SELECT
+    ProductTopCategory,
+    max_ranks - inv_rank rank,
+    sub.Name
+FROM
 (
-	SELECT DISTINCT ProductTopCategory
-	from DM_FactSales fs
-);
-
-SELECT COUNT(*) as timesPurchased
-RANK() OVER (PARTITION BY
-                     p.ProductTopCategory
-                 ORDER BY
-                     COUNT(*)
-                ) ProductRank
-FROM DM_FactSales fs
-LEFT JOIN DM_Product p
-ON fs.ProductID = p.ProductID;
+    SELECT
+        p.ProductTopCategory,
+        p.Name,
+        COUNT(*) sales,
+        ROW_NUMBER() OVER (PARTITION BY p.ProductTopCategory ORDER BY sales) inv_rank,
+        COUNT(*) OVER (PARTITION BY p.ProductTopCategory) max_ranks
+    FROM DM_FactSales fs
+    LEFT JOIN DM_Product p
+    ON fs.ProductID = p.ProductID
+    GROUP BY p.ProductID
+) sub
+WHERE sub.inv_rank <= 3
+ORDER BY ProductTopCategory, rank;
